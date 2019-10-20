2b04cf50e85755037751301fea0c525375fc7677
Oleg Liepkalo <liepkalo.o@gmail.com>
2019-10-20 12:14:22 +0300

    Updates

---
+++ precache-manifest.825ecdf82be831cee94bb8ee62f9d401.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add7b615ea06445fe306e82dda30d048",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "c029c780c59b4c1c8281",
+    "url": "/react/static/css/main.5e10785d.chunk.css"
+  },
+  {
+    "revision": "3b895b5076e54d84c297",
+    "url": "/react/static/js/2.dbcf7199.chunk.js"
+  },
+  {
+    "revision": "c029c780c59b4c1c8281",
+    "url": "/react/static/js/main.d46fa7b4.chunk.js"
+  },
+  {
+    "revision": "76f30fdb11c7a4f7968f",
+    "url": "/react/static/js/runtime-main.bcff5b7a.js"
+  }
+]);
